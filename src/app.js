fd20e3002ec20a9971f7cfd6f21255ff9f51c7da
Evan <evanjensen95@gmail.com>
2021-08-04 15:27:55 -0700

    second

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const fs = require('fs');
-const fs = require('path')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("index", { title: "Index" });
+});
+
+let port = 3000;
+
+app.listen(port, () => {
+  console.log("this dang thinf is running on port 3000");
+});
