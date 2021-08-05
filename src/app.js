6c66f2b08ead24857f05ffda322163a4213beacb
Evan <evanjensen95@gmail.com>
2021-08-05 10:40:30 -0700

    third

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
@@ -8,12 +9,30 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.render("index", { title: "Index" });
+const accountData = fs.readFileSync("src/json/accounts.json", "UTF8");
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync("src/json/users.json", "UTF8");
+const users = JSON.parse(userData);
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Account Summary", accounts: accounts });
+});
+app.get("/savings", (req, res) => {
+  res.render("account", { account: accounts.savings });
+});
+app.get("/checking", (req, res) => {
+  res.render("account", { account: accounts.checking });
+});
+app.get("/credit", (req, res) => {
+  res.render("account", { account: accounts.credit });
+});
+app.get("/transfer", (req, res) => {
+  res.render("transfer");
+});
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: users[0] });
 });
 
-let port = 3000;
-
-app.listen(port, () => {
-  console.log("this dang thinf is running on port 3000");
+app.listen(3000, () => {
+  console.log("PS Project Running on port 3000!");
 });
